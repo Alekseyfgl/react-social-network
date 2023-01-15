b7e34cc9466b4f1f5c0381b34c10b8dd6a545674
aleksey <Aleksey.fgl@gmail.com>
2023-01-15 16:34:26 +0300

    make changes to styles for nav

--- src/components/Main/Navbar/Navbar.tsx
+++ src/components/Main/Navbar/Navbar.tsx
@@ -5,65 +5,57 @@ import { NavLink } from 'react-router-dom';
 
 export const Navbar = () => {
     return (
-        <div className={`${s.menu} block_light-blue`}>
-            <ul>
-                <li className={s.item}>
-                    <NavLink to="/profile" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'user'} />
-                        </span>
-                        <span>My page</span>
-                    </NavLink>
-                </li>
-                <li className={s.item}>
-                    <NavLink to="/news" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'news'} />
-                        </span>
-                        <span>News</span>
-                    </NavLink>
-                </li>
-                <li className={s.item}>
-                    <NavLink to="/acquaintance" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'acquaintance'} />
-                        </span>
-                        <span>Acquaintance</span>
-                    </NavLink>
-                </li>
-                <li className={s.item}>
-                    <NavLink to="/messages" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'messages'} />
-                        </span>
-                        <span>Messages</span>
-                    </NavLink>
-                </li>
-                <li className={s.item}>
-                    <NavLink to="/settings" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'settings'} />
-                        </span>
-                        <span>Settings</span>
-                    </NavLink>
-                </li>
-                <li className={s.item}>
-                    <NavLink to="/developers" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'developers'} />
-                        </span>
-                        <span>Developers</span>
-                    </NavLink>
-                </li>
-                <li className={s.item}>
-                    <NavLink to="/technologies" className={s.link}>
-                        <span className={s.svg_wr}>
-                            <MainSvgSelector id={'technologies'} />
-                        </span>
-                        <span>Technologies</span>
-                    </NavLink>
-                </li>
-            </ul>
+        <div className={`${s.navbar} block_light-blue`}>
+            <nav>
+                <NavLink activeClassName={s.active} to="/profile" className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'user'} />
+                    </span>
+                    <span>My page</span>
+                </NavLink>
+
+                <NavLink to="/news" activeClassName={s.active} className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'news'} />
+                    </span>
+                    <span>News</span>
+                </NavLink>
+
+                <NavLink to="/acquaintance" activeClassName={s.active} className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'acquaintance'} />
+                    </span>
+                    <span>Acquaintance</span>
+                </NavLink>
+
+                <NavLink to="/messages" activeClassName={s.active} className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'messages'} />
+                    </span>
+                    <span>Messages</span>
+                </NavLink>
+
+                <NavLink to="/settings" activeClassName={s.active} className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'settings'} />
+                    </span>
+                    <span>Settings</span>
+                </NavLink>
+
+                <NavLink to="/developers" activeClassName={s.active} className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'developers'} />
+                    </span>
+                    <span>Developers</span>
+                </NavLink>
+
+                <NavLink to="/technologies" activeClassName={s.active} className={s.link}>
+                    <span className={s.svg_wr}>
+                        <MainSvgSelector id={'technologies'} />
+                    </span>
+                    <span>Technologies</span>
+                </NavLink>
+            </nav>
 
             <Ad />
         </div>
